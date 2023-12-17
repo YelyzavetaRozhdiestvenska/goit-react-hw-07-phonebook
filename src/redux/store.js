0c7b168ac6797b5b06a57fbafc569bb232f14043
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-12-17 20:48:35 +0100

    delete percist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
 import { contactsReducer } from './contactsSlice';
 import filterReducer from './filterSlice';
 
@@ -18,13 +8,4 @@ export const store = configureStore({
     contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
 });
-
-export const persistor = persistStore(store);
